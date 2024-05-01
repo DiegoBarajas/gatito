7eb91d29037c381828080e7f40acd088a8879c71
DiegoBarajas <desaubv@gmail.com>
2024-05-01 09:14:02 -0600

    PROD VARIABLES 2

--- front/src/socket.js
+++ front/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
-const URL = process.env.NODE_ENV === 'production' ? "https://gatito-dev-cfeq.1.us-1.fl0.io" : 'http://192.168.100.86:4000';
+const URL = process.env.NODE_ENV === 'production' ? "https://gatito.onrender.com" : 'http://192.168.100.86:4000';
 
 export const socket = io(URL);
 
