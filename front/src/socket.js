6a8a7592d4fa514a6f067222f52cf61d844bc349
Diego Barajas (devsau) <81001205+DiegoBarajas@users.noreply.github.com>
2024-05-01 11:04:48 -0600

    Update socket.js

--- front/src/socket.js
+++ front/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
-const URL = process.env.NODE_ENV === 'production' ? "https://gatito.onrender.com" : 'http://192.168.100.86:4000';
+const URL = process.env.NODE_ENV === 'production' ? "https://gatito.onrender.com" : 'http://localhost:4000';
 
 export const socket = io(URL);
 
