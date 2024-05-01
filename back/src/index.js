16f02aa714bffeac76bd965071795902f1dd8284
DiegoBarajas <desaubv@gmail.com>
2024-05-01 09:04:57 -0600

    v1.0.0 Prod Try 2

--- back/src/index.js
+++ back/src/index.js
@@ -1,4 +1,3 @@
-const fs = require('fs');
 const { server, app, io } = require('./server');
 const color = require('colors/safe');
 const path = require('path');
@@ -7,13 +6,4 @@ require('./sockets/sockets')(io);
 server.listen( app.get('port'), () => {
     console.clear();
     console.log(color.cyan(`[ SERVIDOR ]: Lanzado en el puerto ${app.get('port')}`));
-
-    const folder = path.resolve('./src/rooms')
-    try{
-        fs.rmSync(folder, { recursive: true });
-    }catch(err){
-        console.log(color.red(`[ ERROR ]: No se borraron las rooms: `),err);
-        
-    }
-
 });
