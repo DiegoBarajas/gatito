67f0e931fd14f23bac9a998339e7186d24e83258
DiegoBarajas <desaubv@gmail.com>
2024-05-03 08:17:38 -0600

    V1.0.3

--- front/src/pages/GameRemote.js
+++ front/src/pages/GameRemote.js
@@ -144,32 +144,6 @@ const GameRemote = ({ game, me, gameCode, amIHost }) => {
 
 export default GameRemote;
 
-function checkForWinner( board ){
-    for (let i = 0; i < 3; i++) {
-        if (board[i][0] !== '' && board[i][0] === board[i][1] && board[i][1] === board[i][2]) return board[i][0];
-    }
-
-    for (let i = 0; i < 3; i++) {
-        if (board[0][i] !== '' && board[0][i] === board[1][i] && board[1][i] === board[2][i]) return board[0][i]; 
-    }
-
-    if (board[0][0] !== '' && board[0][0] === board[1][1] && board[1][1] === board[2][2]) return board[0][0];
-    if (board[0][2] !== '' && board[0][2] === board[1][1] && board[1][1] === board[2][0]) return board[0][2];
-    
-    let empate = true;
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-            if (board[i][j] === '') {
-                empate = false;
-                break;
-            }
-        }
-        if (!empate) break;
-    }
-    if (empate) return '0';
-
-    return null;
-}
 function generateBoardType(){
     const numeroAleatorio = Math.floor(Math.random() * 51);
 
@@ -178,31 +152,4 @@ function generateBoardType(){
     else if(numeroAleatorio < 69) return 3
     else if(numeroAleatorio < 92) return 4
     else return 5;
-}
-
-function getWinnerCell(board){
-    for (let i = 0; i < 3; i++) {
-        if (board[i][0] !== '' && board[i][0] === board[i][1] && board[i][1] === board[i][2]) return i+1;
-    }
-    
-    for (let i = 0; i < 3; i++) {
-        if (board[0][i] !== '' && board[0][i] === board[1][i] && board[1][i] === board[2][i]) return i+4; 
-    }
-    
-    if (board[0][0] !== '' && board[0][0] === board[1][1] && board[1][1] === board[2][2]) return 7;
-    if (board[0][2] !== '' && board[0][2] === board[1][1] && board[1][1] === board[2][0]) return 8;
-        
-    let empate = true;
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-            if (board[i][j] === '') {
-                empate = false;
-                break;
-            }
-        }
-        if (!empate) break;
-    }
-    if (empate) return 0;
-    
-    return null;
 }
